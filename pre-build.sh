4a4d4292ee5ffe707a14e3f4544c3cf4ecbc1581
valmy-null <valmyt@gmail.com>
2026-02-28 17:37:35 +0200

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# 1. Поиск всех DTS файлов для Archer C5 V4 и замена лимитов на 16МБ
+# 1. Исправляем лимит в утилите сборки TP-Link (самое важное!)
+find padavan-ng/trunk/vendors/Mediatek -name "Makefile" -exec sed -i 's/7864320/16252928/g' {} +
+
+# 2. Поиск и правка DTS файлов (разметка железа)
 find padavan-ng/trunk/linux-3.4.x/arch/mips/boot/dts -name "*archer-c5-v4*" -type f -exec sed -i 's/0x7c0000/0xfc0000/g' {} +
 find padavan-ng/trunk/linux-3.4.x/arch/mips/boot/dts -name "*archer-c5-v4*" -type f -exec sed -i 's/0x7f0000/0xff0000/g' {} +
 
-# 2. Поиск partitions.config для вашей модели и обновление лимита для скрипта проверки
+# 3. Правка лимитов для скрипта проверки размера
 find padavan-ng/trunk/configs/boards -name "partitions.config" -path "*C5-V4*" -exec sed -i 's/Firmware, 0x30000, .*/Firmware, 0x30000, 16515072/g' {} +
 
-# 3. Принудительная установка флага 16М в конфиге
+# 4. Включаем флаг 16М в конфиге
 sed -i 's/CONFIG_FIRMWARE_SIZE_16M=n/CONFIG_FIRMWARE_SIZE_16M=y/g' padavan-ng/trunk/.config
