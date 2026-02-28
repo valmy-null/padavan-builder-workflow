a895a0bf3bc260990788528a946ffbedb0850651
valmy-null <valmyt@gmail.com>
2026-02-28 17:10:41 +0200

    Enhance pre-build script for Archer C5 V4 automation
    Automate the search and correction of .dts files and partitions.config for Archer C5 V4. Increase firmware size limit in kernel build script.

--- pre-build.sh
+++ pre-build.sh
@@ -1,10 +1,11 @@
-bash
 #!/bin/bash
-# Авто-правка лимита в конфиге
-sed -i 's/Firmware, 0x30000, .*/Firmware, 0x30000, 16515072/g' padavan-ng/trunk/configs/boards/TPLINK/TL_C5-V4/partitions.config
 
-# Авто-правка разделов в DTS (разметка под 16МБ)
-# Внимание: путь ниже может отличаться, проверьте версию ядра (3.4.x или 4.4.x)
-DTS_FILE="padavan-ng/trunk/linux-3.4.x/arch/mips/boot/dts/rt5650_tplink_archer-c5-v4.dts"
-sed -i 's/0x7c0000/0xfc0000/g' $DTS_FILE
-sed -i 's/0x7f0000/0xff0000/g' $DTS_FILE
+# 1. Автоматический поиск и правка всех .dts файлов для Archer C5 V4
+find padavan-ng/trunk/linux-3.4.x/arch/mips/boot/dts -name "*archer-c5-v4*" -exec sed -i 's/0x7c0000/0xfc0000/g' {} +
+find padavan-ng/trunk/linux-3.4.x/arch/mips/boot/dts -name "*archer-c5-v4*" -exec sed -i 's/0x7f0000/0xff0000/g' {} +
+
+# 2. Автоматический поиск и правка partitions.config
+find padavan-ng/trunk/configs/boards -name "partitions.config" -path "*C5-V4*" -exec sed -i 's/Firmware, 0x30000, .*/Firmware, 0x30000, 16515072/g' {} +
+
+# 3. Принудительное увеличение лимита размера образа в скрипте сборки ядра
+sed -i 's/CONFIG_FIRMWARE_SIZE_16M=n/CONFIG_FIRMWARE_SIZE_16M=y/g' padavan-ng/trunk/.config
