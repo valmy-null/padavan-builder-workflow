ec7bd080cf11130473a4bd170515b3c6632db5da
valmy-null <valmyt@gmail.com>
2026-02-28 17:56:35 +0200

    Update firmware size limits and configuration flags

--- pre-build.sh
+++ pre-build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# 1. Исправляем лимит в утилите сборки TP-Link (самое важное!)
-find padavan-ng/trunk/vendors/Mediatek -name "Makefile" -exec sed -i 's/7864320/16252928/g' {} +
 
-# 2. Поиск и правка DTS файлов (разметка железа)
+# 1. Жесткая замена лимита 7.5МБ на 15.5МБ во всех файлах сборщика
+grep -rl "7864320" padavan-ng/trunk/ | xargs sed -i 's/7864320/16252928/g'
+
+# 2. Поиск и правка всех DTS файлов для Archer C5 V4
 find padavan-ng/trunk/linux-3.4.x/arch/mips/boot/dts -name "*archer-c5-v4*" -type f -exec sed -i 's/0x7c0000/0xfc0000/g' {} +
 find padavan-ng/trunk/linux-3.4.x/arch/mips/boot/dts -name "*archer-c5-v4*" -type f -exec sed -i 's/0x7f0000/0xff0000/g' {} +
 
-# 3. Правка лимитов для скрипта проверки размера
+# 3. Обновление лимита в partitions.config
 find padavan-ng/trunk/configs/boards -name "partitions.config" -path "*C5-V4*" -exec sed -i 's/Firmware, 0x30000, .*/Firmware, 0x30000, 16515072/g' {} +
 
-# 4. Включаем флаг 16М в конфиге
+# 4. Принудительный флаг 16М в конфиге
 sed -i 's/CONFIG_FIRMWARE_SIZE_16M=n/CONFIG_FIRMWARE_SIZE_16M=y/g' padavan-ng/trunk/.config
